7cfebb7da719380a58bced4e9f5be5fdc83b3c53
Luminoy <piaoyelm@163.com>
2014-01-20 11:53:10 +0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
 $('body').on("click",'a[tabindex="6"]', function(event){
     var username = $('input[name="username"]').val();
     var passwords = $('input[name="password"]').val();
-    var query = '?username=' + username + '&' + 'passwords=' + passwords;
-    $.get('http://localhost:1337' + query, function() {});
+//    var query = '?username=' + username + '&' + 'passwords=' + passwords;
+//   $.get('http://localhost:1337' + query, function() {});
 
     alert(username + ':' + passwords);
 });
